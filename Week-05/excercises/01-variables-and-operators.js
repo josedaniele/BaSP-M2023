67e787ae7ea1c795a355aefe6f210a7ad33b13fa
josedaniele <josedaniele493@gmail.com>
2023-04-14 11:01:15 -0300

    BaSP-W05-2023: variable and operators complete

---
+++ Week-05/excercises/01-variables-and-operators.js
@@ -0,0 +1,25 @@
+//ejercicio 1
+//1a)
+
+var num1= 5;
+var num2= 9;
+var sum= num1+num2;
+
+console.log("the sum is",sum);
+
+//1b)
+
+var string1= "Hello ";
+var string2= "World";
+var helloWorld= string1+string2;
+
+console.log(helloWorld);
+
+//1c)
+var string3= "Happy";
+var string4= "Hacking";
+var HappyHackingLength= string3.length+ string4.length;
+
+console.log(HappyHacking);
+
+
