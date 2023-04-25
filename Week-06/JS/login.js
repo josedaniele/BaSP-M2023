fd624fb5db3bcba087e129cd0c0bbafab3304afb
josedaniele <josedaniele493@gmail.com>
2023-04-24 23:41:16 -0300

    BaSP-W06-2023: specific error messages in the login changed to a generic one

--- Week-06/JS/login.js
+++ Week-06/JS/login.js
@@ -1,115 +1,82 @@
-
 //select the elements to manipulate
 
-var email = document.querySelector('#email');
-var password = document.querySelector('#password');
-var buttonLogin = document.querySelector('#log-in-button');
+var email = document.querySelector("#email");
+var password = document.querySelector("#password");
+var buttonLogin = document.querySelector("#log-in-button");
 var emailRegex = /^[^@]+@[^@]+.[a-zA-Z]{2,}$/;
 
 // create the events and functions
 
+function emailValidation() {
+  if (email.value === "" || !emailRegex.test(email.value)) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
-
-function emailValidation(){
-    if (email.value ==="") {
-        return 1;
-       
-    }else if (!emailRegex.test(email.value)) {
-        return 2;
-
-    }else{
-        return true;
-
+function passwordValidation() {
+  if (
+    password.value === "" ||
+    password.value.length < 8 ||
+    validateNumbersAndLetters(password.value) == false
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function validateNumbersAndLetters(passwordTest) {
+  var haveNumbers = false;
+  var i = 0;
+  do {
+    if (!isNaN(passwordTest[i])) {
+      haveNumbers = true;
     }
+    i++;
+  } while (i < passwordTest.length && haveNumbers == false);
+
+  if (haveNumbers == true && isNaN(passwordTest)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function passwordValidation(){
-    if (password.value === "") {
-        return 1;
-
-    }else if (password.value.length < 8) {
-        return 2;
-
-    }else if (validateNumbersAndLetters(password.value) == false){
-        return 3;
-
-    }else{
-        return true;
-
-    }
-}
-function validateNumbersAndLetters(passwordTest){
-    var haveNumbers = false;
-    var i =0;
-    do{
-        if (!isNaN(passwordTest[i])) {
-            haveNumbers = true;
-        }
-        i++;
-
-    }while(i < passwordTest.length && haveNumbers== false)
-
-    if (haveNumbers == true && isNaN(passwordTest)){
-        return true;
-    }else{
-        return false;
-    }
+function blurEmail() {
+  if (emailValidation() === false) {
+    password.insertAdjacentHTML(
+      "afterend",
+      '<div id="email-password-error" class="error-message">Email or password incorrect</div>'
+    );
+  }
 }
 
-function blurEmail(){
-    if (emailValidation() === 1) {
-
-        email.insertAdjacentHTML('afterend', '<div id="email-error" class="error-message">Email required</div>');
-
-    }else if (emailValidation() === 2) {
-
-        email.insertAdjacentHTML('afterend', '<div id="email-error" class="error-message">Enter a valid e-mail address(for example juanalberto@gmail.com)</div>');
-   
-    } 
-        
-
+function blurPassword() {
+  if (passwordValidation() === false) {
+    password.insertAdjacentHTML(
+      "afterend",
+      '<div id="email-password-error" class="error-message">Email or password incorrect</div>'
+    );
+  }
 }
 
-function blurPassword(){
-    if (passwordValidation() === 1) {
-
-        password.insertAdjacentHTML('afterend', '<div id="password-error" class="error-message">Password required</div>');
-
-    }else if (passwordValidation() === 2) {
-
-        password.insertAdjacentHTML('afterend', '<div id="password-error" class="error-message">Password must be at least 8 characters</div>');
-        
-    }else if (passwordValidation() === 3) {
-
-        password.insertAdjacentHTML('afterend', '<div id="password-error" class="error-message">Password must contain letters and numbers</div>');
-        
-    }
-
+function focusInputEmailPassword() {
+  if (document.querySelector("#email-password-error")) {
+    document.querySelector("#email-password-error").remove();
+  }
 }
 
-function focusInputEmail(){
-    if (document.querySelector('#email-error')) {
-        document.querySelector('#email-error').remove();
-    }
-    
+function onClickLogIn() {
+  if (emailValidation() && passwordValidation() == true) {
+    alert("Email: " + email.value + "\nPassword: " + password.value);
+  } else {
+    alert("Email or password error");
+  }
 }
 
-function focusInputPassword(){
-    if (document.querySelector('#password-error')) {
-        document.querySelector('#password-error').remove();
-    }
-}
-
-function onClickLogIn(){
-    if (emailValidation() && passwordValidation() == true) {
-        alert('Email: '+ email.value + "\nPassword: " + password.value);
-    }else{
-        alert("Email or password error");
-    }
-}
-
-email.addEventListener("blur" ,blurEmail);
-email.addEventListener("focus",focusInputEmail );
-password.addEventListener("blur" ,blurPassword);
-password.addEventListener("focus",focusInputPassword);
+email.addEventListener("blur", blurEmail);
+email.addEventListener("focus", focusInputEmailPassword);
+password.addEventListener("blur", blurPassword);
+password.addEventListener("focus", focusInputEmailPassword);
 buttonLogin.addEventListener("click", onClickLogIn);
